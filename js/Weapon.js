e4b821f890ff239155f6502aefb11712b7573289
Space <cherif.aichoun@gmail.com>
2019-08-09 13:31:15 +0200

    Cleaning

--- js/Weapon.js
+++ js/Weapon.js
@@ -1,5 +1,5 @@
 class Weapon {
-  constructor (name, damage, skin, skin2, description) {
+  constructor (name, damage, skin, skin2) {
     this.name = name;
     this.skin = skin;
     this.skin2 = skin2;
